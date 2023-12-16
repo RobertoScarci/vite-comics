6e718f922384b45a5e9676298f7876eac3035f73
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-16 20:28:22 +0100

    Modifiche avvenute all'interno dell'App.vue

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,25 +1,22 @@
-<script>
-import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
-import AppFooter from './components/AppFooter.vue';
-
-export default {
-  name: 'App',
-  components:{
-    AppHeader,
-    AppMain,
-    AppFooter
-  }
-}
-</script>
-
-<template lang="">
+<template>
   <AppHeader/>
   <AppMain/>
   <AppFooter/>
 </template>
 
-<style lang="scss">
-  @use './styles/general.scss' as *;
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter,
+    }
+  }
+</script>
 
+<style lang="scss" scoped>
+  @use "./styles/general.scss" as *;
 </style>
