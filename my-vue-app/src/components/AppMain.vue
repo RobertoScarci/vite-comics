a6fbf117ac393723af77c5af0a5c57c22d45f2e6
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-16 20:28:44 +0100

    Implementazione del main con apposito bg e ul

---
+++ my-vue-app/src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <div class="hero"></div>
+        <ListComics/>
+    </main>
+</template>
+
+<script>
+    import ListDc from './ListDc.vue'
+
+    export default {
+        components: {
+            ListDc
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use "../styles/partials/variables" as *;
+    @use "../styles/partials/mixins" as *;
+
+    main {
+        background-color: $list-comics-color;
+        .hero {
+            width: 100%;
+            height: 100px;
+    
+            background-image: url("../assets/img/jumbotron.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    }
+</style>
