26c2b4a5edcc20286371e797792dc86efa7a7e68
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-16 20:29:31 +0100

    implementazione degli appositi elementi anche nel html

---
+++ my-vue-app/src/components/ListDc.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="listComics">
+        <h1>Content goes here</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use "../styles/partials/variables" as *;
+    @use "../styles/partials/mixins" as *;
+    .listComics {
+        background-color: $list-comics-color;
+        padding: 3rem 10rem;
+        h1 {
+            color: white;
+        }
+    }
+</style>
