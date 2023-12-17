c9c852be96c380dfef8de5ca327e2a7c50373de3
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-17 15:39:17 +0100

    aggiunta delle card nella zona apposita dei fumetti con oggetti

--- my-vue-app/src/components/ListDc.vue
+++ my-vue-app/src/components/ListDc.vue
@@ -1,23 +1,152 @@
 <template>
     <div class="listComics">
-        <h1>Content goes here</h1>
+        <h1>Current series</h1>
+        <div class="cardWrapper">
+            <DcCard v-for="comic in DcList" 
+                :thumb="comic.thumb"
+                :price="comic.price"
+                :series="comic.series"
+                :type="comic.type"
+            />
+        </div>
+        <div class="buttonWrapper">
+            <button>Load More</button>
+        </div>
     </div>
 </template>
 
 <script>
+    import DcCard from './SingleCard.vue'
+
     export default {
-        
+    name: "ListComics",
+    components: {
+        DcCard
+    },
+    data() {
+        return {
+            DcList: [
+                    {
+                        "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                        "price": "$19.99",
+                        "series": "Action Comics",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                        "price": "$3.99",
+                        "series": "American Vampire 1976",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                        "price": "$16.99",
+                        "series": "Aquaman",
+                        "type": "graphic novel",
+                    },
+                    {
+                        "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                        "price": "$2.99",
+                        "series": "Batgirl",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                        "price": "$3.99",
+                        "series": "Batman",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                        "price": "$2.99",
+                        "series": "Batman Beyond",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                        "price": "$3.99",
+                        "series": "Batman/Superman",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                        "price": "$4.99",
+                        "series": "Batman/Superman Annual",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                        "price": "$5.99",
+                        "series": "Batman: The Joker War Zone",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                        "price": "$6.99",
+                        "series": "Batman: Three Jokers",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                        "price": "$4.99",
+                        "series": "Batman: White Knight Presents: Harley Quinn",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                        "price": "$16.99",
+                        "series": "Catwoman",
+                        "type": "graphic novel",
+                    },
+                ]
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
     @use "../styles/partials/variables" as *;
     @use "../styles/partials/mixins" as *;
+
     .listComics {
-        background-color: $list-comics-color;
-        padding: 3rem 10rem;
+        width: 100%;
+        margin: 0 auto;
+        margin: 0 1rem;
+
         h1 {
+            display: inline-block;
+            background-color: $main-color;
+            padding: 1rem 1rem;
+            margin-left: 5rem;
+            font-size: 1.4rem;
             color: white;
+            text-transform: uppercase;
+            position: relative;
+            bottom: 44px;
+            
         }
+        .cardWrapper {
+            @include flex(row, center, stretch);
+            flex-wrap: wrap;
+        }
+
+        .buttonWrapper {
+            text-align: center;
+            padding: 1.5rem 0;
+
+            button {
+                border: none;
+                background-color: $main-color;
+                padding: 0.8rem 3rem;
+                color: white;
+                text-transform: uppercase;
+                font-weight: 700;
+            }
+            button:hover{
+                background-color: rgb(108, 108, 241);
+                font-size: 1rem;
+            }
+        }
+        
     }
 </style>
---
+++ my-vue-app/src/components/SingleCard.vue
@@ -0,0 +1,54 @@
+<script>
+    export default {
+        name: "SingleCard",
+        props: {
+            thumb: String,
+            price: String,
+            series: String,
+            type: String,
+        }
+    }
+</script>
+
+<template>
+    <article>
+        <div class="imgWrapper">
+            <img :src="thumb" :alt="series + '\'s thumb'">
+        </div>
+        <p> {{ series }} </p>
+    </article>
+</template>
+
+<style lang="scss" scoped>
+    @use "../styles/partials/variables" as *;
+    @use "../styles/partials/mixins" as *;
+
+    article {
+        width: calc(100% / 6 - 3rem);
+        margin-right: 1rem;
+        margin-bottom: 1rem;
+
+        .imgWrapper {
+            width: 180px;
+            height: 150px;
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: top;
+            }
+            img:hover{
+                opacity: 0.5;
+            }
+        }
+
+        p {
+            margin: 1rem 0;
+            color: white;
+            text-transform: uppercase;
+            font-size: 1rem;
+        }
+
+    }
+</style>
